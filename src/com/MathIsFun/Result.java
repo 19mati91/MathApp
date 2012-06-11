691bdc001867d411f739a2678e1620b6d423deca
agoransson <andreas.goransson@mah.se>
2012-06-11 09:10:16 +0200

    Fixade Result.java, den verkade lite märklig.

--- src/com/MathIsFun/Result.java
+++ src/com/MathIsFun/Result.java
@@ -4,28 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class Result {
+public class Result extends Activity {
 	
 	// Gör en listView för utskriften av de felaktiga svaren
 	// Gör en for loop som loopar genom wronglist och matar in värdena i listView
 	
-	public class NextActivity extends Activity {
-		// Called when the activity is first created.
-		public void onCreate(Bundle savedInstanceState, String counter, String score) {
-			
-			setContentView(R.layout.result);
-			TextView result = (TextView) findViewById(R.id.finale);
-			result.setText("Your final score is: " + score + " out of " + counter);
-			
-			
-		}
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		setContentView(R.layout.result);
 		
-		}
+		TextView result = (TextView) findViewById(R.id.finale);
+		result.setText("Your final score is: " + score + " out of " + counter);
+	}
 	
-	
-	
-	
-
-	
-
 }
